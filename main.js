aa5952db81b89322aa44567174395bc28f20371c
Potato <poltoradnevone@gmail.com>
2022-07-20 18:10:08 +0400

    added contact event

--- main.js
+++ main.js
@@ -48,3 +48,40 @@ const slideObserver = new IntersectionObserver(
 );
 
 slideObserver.observe(slides[slides.length - 1]);
+
+// Form Event
+
+const form = document.querySelector('#contact-form');
+const email = document.querySelector('#email')
+const submitBtn = document.querySelector('#contact-btn');
+
+const contactBtnOptions = {
+  pending: `
+    <svg xmlns="http://www.w3.org/2000/svg" class="animate-spin" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="128" y1="32" x2="128" y2="64" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="224" y1="128" x2="192" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="195.9" y1="195.9" x2="173.3" y2="173.3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="128" y1="224" x2="128" y2="192" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="60.1" y1="195.9" x2="82.7" y2="173.3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="32" y1="128" x2="64" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="60.1" y1="60.1" x2="82.7" y2="82.7" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line></svg>
+    <span class="uppercase tracking-wide animate-pulse">
+    Sending...
+    </span>
+  `,
+  success: `
+  <span class="uppercase tracking-wide">
+    Thank you!
+    </span>
+    <span class="uppercase tracking-wide">
+    ✌️
+    </span>`,
+};
+
+const postEmailToDatabase = (email) => {
+  console.info(`Your email is: ${email}`);
+  return new Promise((resolve) => setTimeout(resolve, 2000))
+}
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  submitBtn.innerHTML = contactBtnOptions.pending;
+  const emailValue = email.value;
+  addDisabledAttribute(submitBtn);
+  email.style.display = 'none';
+  await postEmailToDatabase(emailValue);
+  submitBtn.innerHTML = contactBtnOptions.success;
+});
